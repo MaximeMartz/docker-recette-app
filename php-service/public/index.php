fa6564219899cca3dc2efdfba677997054953cf9
maxime <maxime@maximehebert.fr>
2025-02-05 16:20:05 +0100

    feat: add  options for cors

--- php-service/public/index.php
+++ php-service/public/index.php
@@ -1,10 +1,16 @@
 <?php
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 require_once __DIR__ . '/../src/RecipeController.php';
+
+// Si c'est une requête OPTIONS (pré-vol), on renvoie juste un statut 200
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $uri = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
